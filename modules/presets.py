b7981a7328b4d651edb17f47d6ab2a8e1804f6a2
renmark314 <143886893+renmark314@users.noreply.github.com>
2023-09-05 18:38:25 +0800

    Update presets.py

--- modules/presets.py
+++ modules/presets.py
@@ -44,8 +44,7 @@ INDEX_QUERY_TEMPRATURE = 1.0
 
 CHUANHU_TITLE = i18n("ReonGPT 🚀")
 
-CHUANHU_DESCRIPTION = i18n("由Bilibili [土川虎虎虎](https://space.bilibili.com/29125536)、[明昭MZhao](https://space.bilibili.com/24807452) 和 [Keldos](https://github.com/Keldos-Li) 开发<br />访问川虎Chat的 [GitHub项目](https://github.com/GaiZhenbiao/ChuanhuChatGPT) 下载最新版脚本")
-
+CHUANHU_DESCRIPTION = i18n("Integrating Web Search in GPT")
 
 ONLINE_MODELS = [
     "gpt-3.5-turbo",
